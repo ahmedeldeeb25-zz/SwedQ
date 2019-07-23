83129a13aad32a73f094e2e6a3d8eb81f331b31f
ahmedeldeeb25 <ahmedeldeeb2013@yahoo.com>
2019-07-23 20:15:02 +0200

    final version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "The application will start soon -- Spring-Boot ..." 
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "testpro-0.0.1-SNAPSHOT.war"
+exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar -Dspring.profiles.active=prod "testpro-0.0.1-SNAPSHOT.war"
